4718469490cb9871bb55f5042e820281e1e97a4c
Keven Molina <kevenmolina@gmail.com>
2021-01-05 07:27:12 -0600

    Counter App Code

---
+++ script.js
@@ -0,0 +1,20 @@
+//upon pressing  
+let count = 0;
+let counterContainer= document.querySelector('.counter');
+let Btn = document.querySelectorAll(".ctrl-pnl-btn");
+console.log(Btn.dataset);
+counterContainer.innerText=count;
+// functions
+function increment(){
+    return counterContainer.innerText=count++;
+}
+function decrement(){
+    return counterContainer.innerText=count--;
+}
+function reset(){
+    return counterContainer.innerText=0;
+}
+//  Event Listeners
+Btn[0].addEventListener('click',decrement);
+Btn[1].addEventListener('click',reset);
+Btn[2].addEventListener('click',increment);
